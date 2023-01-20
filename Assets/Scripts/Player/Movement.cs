87eb40a772ed2f8991f6c3bd6bab2db9591af6e7
gebalassa <luciuswrath@hotmail.com>
2023-01-20 00:38:05 -0300

    feat: Idle Direccional (Sprites+Animator+Código) + Inicio "SpaceScene" (sólo copia de MainScene)

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -36,16 +36,16 @@ public class Movement : MonoBehaviour
         horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
         vertical = new Vector3(0.0f, Input.GetAxisRaw("Vertical"), 0.0f);
 
-        if (horizontal == Vector3.zero) horizontal = new Vector3(joystick.Horizontal, 0.0f, 0.0f);
-        if (vertical == Vector3.zero) vertical = new Vector3(0.0f, joystick.Vertical, 0.0f);
+     if (horizontal == Vector3.zero) horizontal = new Vector3(joystick.Horizontal, 0.0f, 0.0f);
+     if (vertical == Vector3.zero) vertical = new Vector3(0.0f, joystick.Vertical, 0.0f);
 
         // Direction logic
-        if (horizontal != new Vector3() && vertical == new Vector3())
+        if (horizontal != Vector3.zero && vertical == Vector3.zero)
         {
             currentDirection = horizontal;
             prevDirection = horizontal;
         }
-        else if (horizontal == new Vector3() && vertical != new Vector3())
+        else if (horizontal == Vector3.zero && vertical != Vector3.zero)
         {
             currentDirection = vertical;
             prevDirection = vertical;
@@ -68,6 +68,23 @@ public class Movement : MonoBehaviour
         // Animation logic
         animator.SetInteger("Horizontal", (int) currentDirection.x);
         animator.SetInteger("Vertical", (int) currentDirection.y);
+        // Gabo - Direccion "Idle"
+        if (currentDirection.x < 0)
+        {
+            animator.SetInteger("Facing", 1);
+        }
+        else if (currentDirection.x > 0)
+        {
+            animator.SetInteger("Facing", 2);
+        }
+        else if (currentDirection.y > 0)
+        {
+            animator.SetInteger("Facing", 3);
+        }
+        else if (currentDirection.y < 0)
+        {
+            animator.SetInteger("Facing", 4);
+        }
 
     }
 }
