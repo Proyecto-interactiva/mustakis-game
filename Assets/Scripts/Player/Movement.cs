c0a16ce31d6b9f78047a055a9b681e9e25c46f7e
gebalassa <luciuswrath@hotmail.com>
2023-01-27 23:05:40 -0300

    feat: Player ahora no se mueve si MessagesDisplay o el QuestionBox están abiertos (isMovable, CheckMovable)

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class Movement : MonoBehaviour
 {
     // Configuration
     public float speed = 0.5f;
+    [NonSerialized]
+    public bool isMovable = false;
 
     // Movement vectors
     Vector3 horizontal;
@@ -16,28 +19,57 @@ public class Movement : MonoBehaviour
     // Physics movement
     public Rigidbody2D rb;
 
-    public Joystick joystick;
-    
+    // Joystick
+    public Joystick joystick;    
 
     // Animation components
     public Animator animator;
+
+    // Gabo - Objetos GUI a chequear si están activos/inactivos (ej.: Para bloquear movimiento)
+    private MessagesDisplay messagesDisplay;
+    private UIQuestionBox questionBox;
 
 
     // Start is called before the first frame update
     void Start()
     {
         prevDirection = new Vector3();
+           
+        // Encontrar objetos GUI chequeables
+        messagesDisplay = FindObjectOfType<MessagesDisplay>(true);
+        if (messagesDisplay == null) { Debug.LogError("Movement: 'MessagesDisplay' no encontrado!"); }
+        questionBox = FindObjectOfType<UIQuestionBox>(true);
+        if (questionBox == null) { Debug.LogError("Movement: 'UIQuestionBox' no encontrado!"); }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Se puede mover sí, y sólo sí, CheckMovable retorna verdadero.
+        isMovable = CheckMovable();
+        if (isMovable)
+        {
+            Move();
+        }
+        else
+        {
+            // Detiene movimiento (Ej.: Del frame anterior)
+            rb.velocity = Vector2.zero;
+            // Detiene animación y transiciona a algún Idle
+            animator.SetInteger("Horizontal", 0);
+            animator.SetInteger("Vertical", 0);
+        }
+    }
+
+    // Movimiento
+    private void Move()
+    {
         currentDirection = new Vector3();
         horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
         vertical = new Vector3(0.0f, Input.GetAxisRaw("Vertical"), 0.0f);
 
-     if (horizontal == Vector3.zero) horizontal = new Vector3(joystick.Horizontal, 0.0f, 0.0f);
-     if (vertical == Vector3.zero) vertical = new Vector3(0.0f, joystick.Vertical, 0.0f);
+        if (horizontal == Vector3.zero) horizontal = new Vector3(joystick.Horizontal, 0.0f, 0.0f);
+        if (vertical == Vector3.zero) vertical = new Vector3(0.0f, joystick.Vertical, 0.0f);
 
         // Direction logic
         if (horizontal != Vector3.zero && vertical == Vector3.zero)
@@ -66,9 +98,9 @@ public class Movement : MonoBehaviour
         rb.velocity = new Vector2(currentDirection.x, currentDirection.y) * speed;
 
         // Animation logic
-        animator.SetInteger("Horizontal", (int) currentDirection.x);
-        animator.SetInteger("Vertical", (int) currentDirection.y);
-        // Gabo - Direccion "Idle"
+        animator.SetInteger("Horizontal", (int)currentDirection.x);
+        animator.SetInteger("Vertical", (int)currentDirection.y);
+        // Gabo - Facing (afecta direccion "Idle")
         if (currentDirection.x < 0)
         {
             animator.SetInteger("Facing", 1);
@@ -85,6 +117,12 @@ public class Movement : MonoBehaviour
         {
             animator.SetInteger("Facing", 4);
         }
+    }
 
+    // Revisa si es legal moverse. Depende de que questionBox y messagesDisplay estén desactivados.
+    private bool CheckMovable()
+    {
+        if (questionBox.isActiveAndEnabled || messagesDisplay.isActiveAndEnabled) { return false; }
+        else { return true; }
     }
 }
